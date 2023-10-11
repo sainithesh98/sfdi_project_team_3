7568b552d4d5cf3dbecc6e1a4d590a24b070b1a4
Sai Nithesh R <sainithesh98@gmail.com>
2023-10-11 17:12:58 -0400

    Plotting changes

--- sfdi_project_team_3.R
+++ sfdi_project_team_3.R
@@ -1,5 +1,12 @@
-#Data Visualization 
+#Data Collection
 data = read.csv("train.csv")
 View(data)
 str(data)
 summary(data)
+library(tidyverse)
+ggplot(data,aes(MSZoning))+
+  geom_bar()
+#Alley can be removed from the dataset as most of it is NA
+ggplot(data,aes(Alley))+
+  geom_bar()
+cor(data)
